45c546c20092d7367c1a13020a5982da8b5d03f0
Santiago Mondragon <santiago.mondragon224@gmail.com>
2024-05-31 14:24:22 -0700

    Calculate string length when printing name

---
+++ programs/lib/calc_str_length.asm
@@ -0,0 +1,28 @@
+	section	.text
+	global	calc_str_length
+
+calc_str_length:
+	; Preserve r13
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 0x40
+	mov	[rsp-0x40], r13
+
+	mov	rcx, 0		; counter
+
+loop:
+	mov	r13, [rdi+rcx]	; Move mystr to counter offset
+	movzx	r13, r13b	; Get next char
+	cmp	r13, 0		; Should be 0 when we go past str size
+	Je	exit
+
+	inc	rcx
+	Jmp	loop
+
+exit:
+	mov	rax, rcx	; rcx stores str length at this point
+	; Restore r13
+	mov	r13, [rsp-0x40]
+	add	rsp, 0x40
+	pop	rbp
+	ret
--- programs/print_name/print_name.asm
+++ programs/print_name/print_name.asm
@@ -1,39 +1,49 @@
 	section .data
-msg:	db	"Enter your name: "	; 17 bytes
-hello_msg: db	"Hello, "		; 7 bytes
-name:	db	15			; 15 bytes
+msg:	db	"Enter your name (32 chars max): ", 0
+hello_msg: db	"Hello, ", 0
+
+	section	.bss
+name:	resd	1
 
 	section	.text
-	global _start
+	global	_start
+	extern	calc_str_length
 
 _start:	; Prompt user for name
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 17
+	mov	rdi, msg
+	call	calc_str_length
+	mov	rdx, rax
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, msg
 	syscall
 
-	; Get name from user (15 chars max)
-	mov rax, 0
-	mov rdi, 0
-	mov rsi, name
-	mov rdx, 15
+	; Get name from user (32 chars max)
+	mov	rax, 0
+	mov	rdi, 0
+	mov	rsi, name
+	mov	rdx, 0x20
 	syscall
 
 	; Say hello to user
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, hello_msg
-	mov rdx, 7
+	mov	rdi, hello_msg
+	call	calc_str_length
+	mov	rdx, rax
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, hello_msg
 	syscall
 
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, name
-	mov rdx, 15
+	; Print name
+	mov	rdi, name
+	call	calc_str_length
+	mov	rdx, rax
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, name
 	syscall
 
 	; Exit program
-	mov rax, 60
-	mov rdi, 0
+	mov	rax, 60
+	mov	rdi, 0
 	syscall
