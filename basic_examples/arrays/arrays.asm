c590e56e5ef0a3e366cd8ccd3743be32e0621f49
Santiago Mondragon <santiago.mondragon224@gmail.com>
2024-05-31 15:16:54 -0700

    Arrays example with Fibonacci sequence

---
+++ basic_examples/arrays/arrays.asm
@@ -0,0 +1,32 @@
+	section	.data
+arr:	dd	0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+nl:	db	'', 10
+
+	section	.bss
+char:	resb	1
+
+	section	.text
+	global	_start
+	extern	print_int
+
+_start:
+	mov	r15, 0	; Counter
+	mov	rbx, 13	; Array length
+
+loop:
+	cmp	r15, rbx
+	Je exit
+
+	; Store next int in r13d
+	mov	r13d, [arr+r15*4]	; Offset = counter * 4 bytes (32 bits)
+
+	mov	edi, r13d
+	call	print_int
+
+	inc r15
+	Jmp loop
+
+exit:
+	mov	rax, 60
+	mov	rdi, 0
+	syscall
---
+++ basic_examples/lib/print_int.asm
@@ -0,0 +1,68 @@
+; Print integer to console
+
+	section	.bss
+intchar:
+	resb	1
+myint:	resd	1
+
+	section	.text
+	global	print_int
+	extern	print_nl
+
+print_int:
+	; Preserve r15d
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 0x20
+	mov	[rsp-0x20], r15d
+
+	mov	[myint], rdi
+	mov	rdx, 0
+	mov	r12d, 0		; Counter for numbers in stack
+	mov	r15d, 10	; Divisor
+
+loop:	; Push all digits to stack
+	mov	eax, [myint]
+	div	r15d		; Divide myint by 10 to get rightmost digit
+
+	push	rdx		; Rightmost digit is remainder; Push to stack
+	mov	rdx, 0		; Clear rdx
+	inc	r12d
+
+	; Update myint
+	mov	[myint], eax
+
+	; Exit if eax is 0 (means we added all digits to stack)
+	cmp	eax, 0
+	Je	loop2init
+	Jmp	loop
+
+loop2init:	; Print each digit in the stack to the console
+	mov	r15d, 0		; Counter for digits printed
+
+loop2:
+	cmp	r15d, r12d	; Exit once all digits have been printed
+	Je return
+
+	; Get next digit in stack
+	pop	rax
+	add	rax, '0'	; Add ascii offset
+	mov	[intchar], rax
+
+	; Print digit to console
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, intchar
+	mov	rdx, 1
+	syscall
+
+	inc	r15d
+	Jmp loop2
+
+return:
+	call	print_nl
+	; Restore r15d
+	mov	r15d, [rsp-0x20]
+	add	rsp, 0x20
+	pop	rbp
+	ret
