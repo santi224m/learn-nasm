a3dedc7badfd6484ebce81949ab6f7c20b82072b
Santiago Mondragon <santiago.mondragon224@gmail.com>
2024-06-10 14:00:54 -0700

    Implement binary search

--- data_structures/binary_search/binary_search.asm
+++ data_structures/binary_search/binary_search.asm
@@ -1,31 +1,68 @@
 	section	.data
 idxL:	dd	0
-idx:	dd	-1
 
 	section	.bss
+idx:	resd	1
 idxH:	resd	1
+arr:	resd	10
+target:	resd	1
 
 	section	.text
 	global	binary_search
 
 binary_search:
 	; Store 1st arg in idxH
+	mov	r15, rax
+	mov	[target], rdi
+	dec	rsi
 	mov	[idxH], rsi
 
 loop:
-	; Exit when idxL <= idxH
-	mov	r13, [idxL]
-	mov	r14, [idxH]
-	cmp	r13, r14
-	Jle	exit
+	; Exit when idxL > idxH
+	mov	r13d, [idxL]
+	mov	r14d, [idxH]
+	cmp	r14d, r13d
+	Jl	exit
 
 	; Get middle integer -- Store in rbx
-	mov	rax, idxH
-	mov	r14, idxL
-	add	rax, r14
-	div	r14
-	mov	rbx, r14
+	mov	eax, [idxH]
+	mov	r14d, [idxL]
+	add	eax, r14d
+	mov	r14d, 2
+	mov	rdx, 0		; Reset rdx
+	div	r14d
+	mov	ebx, eax
+	mov	[idx], ebx
+
+	; Store mid int value in ebx
+	mov	eax, 4
+	imul	eax, ebx
+	mov	ebx, [r15d+eax]
+
+checkEq:
+	; Check if curr int is target
+	mov	eax, [target]
+	cmp	eax, ebx
+	Jne	checkL
+	mov	eax, [idx]
+	ret
+
+checkL:
+	; Check if curr is less than target
+	cmp	ebx, eax
+	Jnl	checkG
+	mov	eax, [idx]
+	inc	eax
+	mov	[idxL], eax
+	jmp	loop
+
+checkG:
+	; Check if curr is greater than target
+	mov	eax, [idx]
+	dec	eax
+	mov	[idxH], eax
+	Jmp	loop
 
 exit:
-	mov	rax, [idx]
+	mov	rax, -1
 	ret
--- data_structures/binary_search/main.asm
+++ data_structures/binary_search/main.asm
@@ -1,14 +1,41 @@
 	section	.data
 arr:	dd	10, 13, 33, 34, 55, 56, 65, 66, 68, 93
-target:	dd	55
+target:	dd	13
+nl:	db	'', 10
 
 	section	.bss
 idx:	resd	1
+char:	resb	1
 
 	section	.text
 	global	_start
+	extern	binary_search
 
 _start:
+	; Get target index
+	mov	rax, arr
+	mov	rdi, [target]
+	mov	rsi, 10
+	call	binary_search
+	mov	[idx], rax
+
+	; Print index
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, [idx]
+	add	rsi, '0'
+	mov	[char], rsi
+	mov	rsi, char
+	mov	rdx, 1
+	syscall
+
+	; Print newline
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, nl
+	mov	rdx, 1
+	syscall
+
 	; Exit
 	mov	rax, 60
 	mov	rdi, 0
